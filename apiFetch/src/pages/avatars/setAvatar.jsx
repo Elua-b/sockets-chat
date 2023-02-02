87d8f85ba4f6456d9bf92be2f5d0a77e2bdf220c
Elua-b <bugingoeloi@gmail.com>
2023-02-02 11:54:23 +0200

    adding some functionalities

--- apiFetch/src/pages/avatars/setAvatar.jsx
+++ apiFetch/src/pages/avatars/setAvatar.jsx
@@ -4,16 +4,42 @@ import { useState } from "react";
 import axios from "axios";
 import { Buffer } from "Buffer";
 import './setAvatar.css'
+import { setAvatarRoute } from "../../utils/ApiRooute";
 const setAvatar = () => {
   const api = `https://api.multiavatar.com/4645646`;
   const [avatars, setAvatars] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
   const [selectedAvatar, setSelectedAvatar] = useState(undefined);
-  const setProfilePicture = async () => {};
+  const setProfilePicture = async () => {
+    console.log("working");
+    if (selectedAvatar === undefined) {
+     alert("not selected");
+    } else {
+      const user = await JSON.parse(
+        localStorage.getItem("http://localhost:5000")
+      );
+
+      const { data } = await axios.post(`${setAvatarRoute}/${user._id}`, {
+        image: avatars[selectedAvatar],
+      });
+
+      if (data.isSet) {
+        user.isAvatarImageSet = true;
+        user.avatarImage = data.image;
+        localStorage.setItem(
+          "http://localhost:5000",
+          JSON.stringify(user)
+        );
+       console.log("doneeee");
+      } else {
+       console.log("failed try again");
+      }
+    }
+  };
   useEffect(() => {
     const fetchData = async () => {
-        setIsLoading(true);
+        
         for(let i = 0; i < 4; i++) {
         try {
 
@@ -24,6 +50,7 @@ const setAvatar = () => {
             const stringFromBuffer = buffer.toString("base64");
             avatars.push(stringFromBuffer)
             setAvatars([...avatars])
+            setIsLoading(false)
             
           }catch(error) {
             // throw error
@@ -38,9 +65,16 @@ const setAvatar = () => {
     // console.log(avatars)
     fetchData();
   }, []);
-  useEffect(() => {console.log(avatars)},[avatars])
+  // useEffect(() => {console.log(avatars)},[avatars])
   return (
-    <div className="container">
+    <>
+    {
+      isLoading ? (
+        <div className="container">
+          <img src="./loader.gif" alt="" />
+        </div>
+      ):(
+<div className="container">
       <div className="title-container">
         <h1>Pick an avatar</h1>
       </div>
@@ -49,21 +83,26 @@ const setAvatar = () => {
           return (
             <div
             key={index}
-              className={`avatar ${selectedAvatar === index ? "selected" : ""}`}
-           
+            className={`avatar ${selectedAvatar === index ? "selected" : ""}`}
+            
             >
                <img
                     src={`data:image/svg+xml;base64,${avatar}`}
                     alt="avatar"
                     key={avatar}
                     onClick={() => setSelectedAvatar(index)}
-                  />
+                    />
               
             </div>
           );
         })}
       </div>
+      <button className="submit-btn" onClick={setProfilePicture}> Set  as Profile Picture</button>
     </div>
+      )
+    }
+    
+        </>
   );
 };
 
